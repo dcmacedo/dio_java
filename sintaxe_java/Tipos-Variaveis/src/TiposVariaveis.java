fffa5b15d3839a2ea0ba050490ee585138f15aa1
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2024-06-26 23:33:56 -0300

    feat:concluido

--- sintaxe_java/Tipos-Variaveis/src/TiposVariaveis.java
+++ sintaxe_java/Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2 + salarioMinimo);
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
