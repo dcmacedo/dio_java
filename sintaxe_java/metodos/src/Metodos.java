8d0f9900e987fb79ff4105acf86a2cc08fac4502
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2024-06-29 14:58:13 -0300

    :sparkles:feat: Trabalhando com métodos

--- sintaxe_java/metodos/src/Metodos.java
+++ sintaxe_java/metodos/src/Metodos.java
@@ -1,5 +1,24 @@
 public class Metodos {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada?: " + smartTv.ligada);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada?: " + smartTv.ligada);
+        System.out.println("Volume atual: " + smartTv.volume);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV Ligada?: " + smartTv.ligada);
     }
 }
