0d2e26304f33803b2752e73c74600aa13350d26e
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2024-07-01 19:44:11 -0300

    :sparkles:feat: Condicional Encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,12 @@
 public class ResultadoEscolar {
 
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 4;
 
         if (nota >= 7) {
             System.out.println("Aprovado");
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Prova de Recuperação");
         } else {
             System.out.println("Reprovado");
         }
