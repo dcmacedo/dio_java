04e128af1e484b3a56735f49fd05db658e997844
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2024-07-01 20:48:45 -0300

    :sparkles:feat: Condicional Ternário

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -3,12 +3,17 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 4;
 
-        if (nota >= 7) {
-            System.out.println("Aprovado");
-        } else if (nota >= 5 && nota < 7) {
-            System.out.println("Prova de Recuperação");
-        } else {
-            System.out.println("Reprovado");
-        }
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
+        /*
+         * // if (nota >= 7) {
+         * // System.out.println("Aprovado");
+         * // } else if (nota >= 5 && nota < 7) {
+         * // System.out.println("Prova de Recuperação");
+         * // } else {
+         * // System.out.println("Reprovado");
+         * // }
+         */
     }
 }
