d96b2dcb8d2c80a8f4b08eea54e76a635e44ae00
dcmacedo <dcotrimmacedo@gmail.com>
2024-08-03 18:55:43 -0300

    Resolvendo desafio CatalogoLivros e OrdenacaoPessoa

--- collections-java-api-2023/src/Main.java
+++ collections-java-api-2023/src/Main.java
@@ -1,10 +1,12 @@
 import list.OperacoesBasicas.ListaTarefa;
+import list.Ordenacao.OrdenacaoPessoa;
+import list.Pesquisa.CatalogoLivros;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ListaTarefa listaTarefa = new ListaTarefa();
+      /*  ListaTarefa listaTarefa = new ListaTarefa();
         System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
 
         listaTarefa.adicionarTarefa("Tarefa 1");
@@ -16,5 +18,25 @@ public class Main {
         System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
 
         listaTarefa.obterDescricoesTarefas();
+
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1","Autor 1",2020);
+        catalogoLivros.adicionarLivro("Livro 1","Autor 2",2021);
+        catalogoLivros.adicionarLivro("Livro 2","Autor 2",2022);
+        catalogoLivros.adicionarLivro("Livro 3","Autor 3",2023);
+        catalogoLivros.adicionarLivro("Livro 4","Autor 4",1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 4"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020,2021));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));*/
+
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20,1.56);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30,1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25,1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 17,1.56);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
     }
 }
